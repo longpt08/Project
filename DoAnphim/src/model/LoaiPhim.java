e560589a53dd1dc9e8f705a7b51823be252e1f79
Phan Thành Long <66318694+longpt08@users.noreply.github.com>
2020-06-10 14:22:46 +0700

    Create LoaiPhim.java

---
+++ DoAnphim/src/model/LoaiPhim.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author PC
+ */
+public class LoaiPhim {
+    private String MaLP, TenLP, HinhThuc;
+
+    public String getMaLP() {
+        return MaLP;
+    }
+
+    public void setMaLP(String MaLP) {
+        this.MaLP = MaLP;
+    }
+
+    public String getTenLP() {
+        return TenLP;
+    }
+
+    public void setTenLP(String TenLP) {
+        this.TenLP = TenLP;
+    }
+
+    public String getHinhThuc() {
+        return HinhThuc;
+    }
+
+    public void setHinhThuc(String HinhThuc) {
+        this.HinhThuc = HinhThuc;
+    }
+    
+}
