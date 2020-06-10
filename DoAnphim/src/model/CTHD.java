42166270303a7bfb36eb0cfacc30ac3bbd1219ea
Phan Thành Long <66318694+longpt08@users.noreply.github.com>
2020-06-10 14:15:39 +0700

    Create CTHD.java

---
+++ DoAnphim/src/model/CTHD.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author PC
+ */
+public class CTHD {
+    private String MaSP, MaKM;
+            int SoHD;
+            long ThanhTien;
+
+    public String getMaSP() {
+        return MaSP;
+    }
+
+    public void setMaSP(String MaSP) {
+        this.MaSP = MaSP;
+    }
+
+    public String getMaKM() {
+        return MaKM;
+    }
+
+    public void setMaKM(String MaKM) {
+        this.MaKM = MaKM;
+    }
+
+    public int getSoHD() {
+        return SoHD;
+    }
+
+    public void setSoHD(int SoHD) {
+        this.SoHD = SoHD;
+    }
+
+    public long getThanhTien() {
+        return ThanhTien;
+    }
+
+    public void setThanhTien(long ThanhTien) {
+        this.ThanhTien = ThanhTien;
+    }
+            
+}
