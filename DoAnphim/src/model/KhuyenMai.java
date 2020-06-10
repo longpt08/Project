01a92631c6fdb9ddd544624bdf2b04d14fb14a44
Phan Thành Long <66318694+longpt08@users.noreply.github.com>
2020-06-10 14:21:55 +0700

    Create KhuyenMai.java

---
+++ DoAnphim/src/model/KhuyenMai.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author PC
+ */
+public class KhuyenMai {
+    private String MaKM, TenKM;
+            int TyLeKM;
+
+    public String getMaKM() {
+        return MaKM;
+    }
+
+    public void setMaKM(String MaKM) {
+        this.MaKM = MaKM;
+    }
+
+    public String getTenKM() {
+        return TenKM;
+    }
+
+    public void setTenKM(String TenKM) {
+        this.TenKM = TenKM;
+    }
+
+    public int getTyLeKM() {
+        return TyLeKM;
+    }
+
+    public void setTyLeKM(int TyLeKM) {
+        this.TyLeKM = TyLeKM;
+    }
+            
+}
