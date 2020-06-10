24d3e61fac8a99b8c0905fa836257b060ada48cd
Phan Thành Long <66318694+longpt08@users.noreply.github.com>
2020-06-10 14:40:24 +0700

    Create SuatChieu.java

---
+++ DoAnphim/src/model/SuatChieu.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author PC
+ */
+public class SuatChieu {
+    private String MaSC, MaRap, MaPhim, NgayChieu, ThoiGianChieu;
+
+    public String getMaSC() {
+        return MaSC;
+    }
+
+    public void setMaSC(String MaSC) {
+        this.MaSC = MaSC;
+    }
+
+    public String getMaRap() {
+        return MaRap;
+    }
+
+    public void setMaRap(String MaRap) {
+        this.MaRap = MaRap;
+    }
+
+    public String getMaPhim() {
+        return MaPhim;
+    }
+
+    public void setMaPhim(String MaPhim) {
+        this.MaPhim = MaPhim;
+    }
+
+    public String getNgayChieu() {
+        return NgayChieu;
+    }
+
+    public void setNgayChieu(String NgayChieu) {
+        this.NgayChieu = NgayChieu;
+    }
+
+    public String getThoiGianChieu() {
+        return ThoiGianChieu;
+    }
+
+    public void setThoiGianChieu(String ThoiGianChieu) {
+        this.ThoiGianChieu = ThoiGianChieu;
+    }
+    
+}
