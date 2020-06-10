0cbe64c2acaf0072f7164a2f819122c0ff64573d
Phan Thành Long <66318694+longpt08@users.noreply.github.com>
2020-06-10 14:13:57 +0700

    Create BaoCao.java

---
+++ DoAnphim/src/model/BaoCao.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author PC
+ */
+public class BaoCao {
+    private String MaBC, MaNV, NgayLap, TuNgay, DenNgay, GhiChu;
+            long TongDoanhThu;
+
+    public String getMaBC() {
+        return MaBC;
+    }
+
+    public void setMaBC(String MaBC) {
+        this.MaBC = MaBC;
+    }
+
+    public String getMaNV() {
+        return MaNV;
+    }
+
+    public void setMaNV(String MaNV) {
+        this.MaNV = MaNV;
+    }
+
+    public String getNgayLap() {
+        return NgayLap;
+    }
+
+    public void setNgayLap(String NgayLap) {
+        this.NgayLap = NgayLap;
+    }
+
+    public String getTuNgay() {
+        return TuNgay;
+    }
+
+    public void setTuNgay(String TuNgay) {
+        this.TuNgay = TuNgay;
+    }
+
+    public String getDenNgay() {
+        return DenNgay;
+    }
+
+    public void setDenNgay(String DenNgay) {
+        this.DenNgay = DenNgay;
+    }
+
+    public String getGhiChu() {
+        return GhiChu;
+    }
+
+    public void setGhiChu(String GhiChu) {
+        this.GhiChu = GhiChu;
+    }
+
+    public long getTongDoanhThu() {
+        return TongDoanhThu;
+    }
+
+    public void setTongDoanhThu(long TongDoanhThu) {
+        this.TongDoanhThu = TongDoanhThu;
+    }
+            
+}
