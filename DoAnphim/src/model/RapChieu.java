67b6bcda5b46d76df78d6761a2b535b864e45af6
Phan Thành Long <66318694+longpt08@users.noreply.github.com>
2020-06-10 14:37:51 +0700

    create RapChieu.java

---
+++ DoAnphim/src/model/RapChieu.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author PC
+ */
+public class RapChieu {
+    private String MaRap;
+    private int SLGhe;
+
+    public String getMaRap() {
+        return MaRap;
+    }
+
+    public void setMaRap(String MaRap) {
+        this.MaRap = MaRap;
+    }
+
+    public int getSLGhe() {
+        return SLGhe;
+    }
+
+    public void setSLGhe(int SLGhe) {
+        this.SLGhe = SLGhe;
+    }
+    
+}
