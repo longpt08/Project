dc40deaca4050c10d248aef6f53b2452331e34b4
Phan Thành Long <66318694+longpt08@users.noreply.github.com>
2020-06-10 14:39:22 +0700

    Update SanPham.java

--- DoAnphim/src/model/SanPham.java
+++ DoAnphim/src/model/SanPham.java
@@ -10,5 +10,31 @@ package model;
  * @author PC
  */
 public class SanPham {
+    private String MaSP, TenSP;
+    private int Gia;
+
+    public String getMaSP() {
+        return MaSP;
+    }
+
+    public void setMaSP(String MaSP) {
+        this.MaSP = MaSP;
+    }
+
+    public String getTenSP() {
+        return TenSP;
+    }
+
+    public void setTenSP(String TenSP) {
+        this.TenSP = TenSP;
+    }
+
+    public int getGia() {
+        return Gia;
+    }
+
+    public void setGia(int Gia) {
+        this.Gia = Gia;
+    }
     
 }
