e61b6407722a526da3a8fbbe6b316eb71324d94c
Phan Thành Long <66318694+longpt08@users.noreply.github.com>
2020-06-10 14:19:28 +0700

    Create Ghe.java

---
+++ DoAnphim/src/model/Ghe.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author PC
+ */
+public class Ghe {
+    private String MaGhe, MaRap, ViTri, LoaiGhe;
+            long Gia;
+
+    public String getMaGhe() {
+        return MaGhe;
+    }
+
+    public void setMaGhe(String MaGhe) {
+        this.MaGhe = MaGhe;
+    }
+
+    public String getMaRap() {
+        return MaRap;
+    }
+
+    public void setMaRap(String MaRap) {
+        this.MaRap = MaRap;
+    }
+
+    public String getViTri() {
+        return ViTri;
+    }
+
+    public void setViTri(String ViTri) {
+        this.ViTri = ViTri;
+    }
+
+    public String getLoaiGhe() {
+        return LoaiGhe;
+    }
+
+    public void setLoaiGhe(String LoaiGhe) {
+        this.LoaiGhe = LoaiGhe;
+    }
+
+    public long getGia() {
+        return Gia;
+    }
+
+    public void setGia(long Gia) {
+        this.Gia = Gia;
+    }
+            
+}
