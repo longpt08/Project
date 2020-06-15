85715b0022ea768dd1b50bec2907e4a93c9b05c6
Phan Thành Long <66318694+longpt08@users.noreply.github.com>
2020-06-15 09:53:00 +0700

    Data insertion

---
+++ data.sql
@@ -0,0 +1,212 @@
+---------------------------------
+----- GHE - RAP 1
+---------------------------------
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a1r1','1r','ha c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a2r1','1r','ha c2','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a3r1','1r','ha c3','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a4r1','1r','ha c4','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a5r1','1r','ha c5','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a6r1','1r','ha c6','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b1r1','1r','hb c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b2r1','1r','hb c2','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b3r1','1r','hb c3','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b4r1','1r','hb c4','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b5r1','1r','hb c5','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b6r1','1r','hb c6','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c1r1','1r','hc c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c2r1','1r','hc c2','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c3r1','1r','hc c3','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c4r1','1r','hc c4','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c5r1','1r','hc c5','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c6r1','1r','hc c6','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d1r1','1r','hd c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d2r1','1r','hd c2','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d3r1','1r','hd c3','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d4r1','1r','hd c4','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d5r1','1r','hd c5','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d6r1','1r','hd c6','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e1r1','1r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e2r1','1r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e3r1','1r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e4r1','1r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e5r1','1r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e6r1','1r','he c1','thuong',65000);
+
+--------------------------------------------
+--- GHE - RAP 2 
+--------------------------------------------
+
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a1r2','2r','ha c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a2r2','2r','ha c2','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a3r2','2r','ha c3','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a4r2','2r','ha c4','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a5r2','2r','ha c5','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a6r2','2r','ha c6','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b1r2','2r','hb c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b2r2','2r','hb c2','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b3r2','2r','hb c3','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b4r2','2r','hb c4','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b5r2','2r','hb c5','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b6r2','2r','hb c6','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c1r2','2r','hc c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c2r2','2r','hc c2','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c3r2','2r','hc c3','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c4r2','2r','hc c4','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c5r2','2r','hc c5','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c6r2','2r','hc c6','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d1r2','2r','hd c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d2r2','2r','hd c2','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d3r2','2r','hd c3','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d4r2','2r','hd c4','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d5r2','2r','hd c5','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d6r2','2r','hd c6','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e1r2','2r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e2r2','2r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e3r2','2r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e4r2','2r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e5r2','2r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e6r2','2r','he c1','thuong',65000);
+
+-----------------------
+------ GHE - RAP 3-----
+-----------------------
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a1r3','3r','ha c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a2r3','3r','ha c2','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a3r3','3r','ha c3','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a4r3','3r','ha c4','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a5r3','3r','ha c5','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a6r3','3r','ha c6','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b1r3','3r','hb c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b2r3','3r','hb c2','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b3r3','3r','hb c3','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b4r3','3r','hb c4','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b5r3','3r','hb c5','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b6r3','3r','hb c6','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c1r3','3r','hc c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c2r3','3r','hc c2','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c3r3','3r','hc c3','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c4r3','3r','hc c4','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c5r3','3r','hc c5','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c6r3','3r','hc c6','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d1r3','3r','hd c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d2r3','3r','hd c2','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d3r3','3r','hd c3','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d4r3','3r','hd c4','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d5r3','3r','hd c5','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d6r3','3r','hd c6','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e1r3','3r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e2r3','3r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e3r3','3r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e4r3','3r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e5r3','3r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e6r3','3r','he c1','thuong',65000);
+
+--------------------
+---GHE- RAP 4---
+----------------------
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a1r4','4r','ha c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a2r4','4r','ha c2','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a3r4','4r','ha c3','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a4r4','4r','ha c4','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a5r4','4r','ha c5','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a6r4','4r','ha c6','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b1r4','4r','hb c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b2r4','4r','hb c2','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b3r4','4r','hb c3','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b4r4','4r','hb c4','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b5r4','4r','hb c5','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b6r4','4r','hb c6','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c1r4','4r','hc c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c2r4','4r','hc c2','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c3r4','4r','hc c3','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c4r4','4r','hc c4','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c5r4','4r','hc c5','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c6r4','4r','hc c6','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d1r4','4r','hd c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d2r4','4r','hd c2','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d3r4','4r','hd c3','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d4r4','4r','hd c4','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d5r4','4r','hd c5','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d6r4','4r','hd c6','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e1r4','4r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e2r4','4r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e3r4','4r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e4r4','4r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e5r4','4r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e6r4','4r','he c1','thuong',65000);
+
+------------------------------------
+----------- GHE - RAP 5
+-------------------------------------
+
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a1r5','5r','ha c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a2r5','5r','ha c2','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a3r5','5r','ha c3','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a4r5','5r','ha c4','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a5r5','5r','ha c5','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('a6r5','5r','ha c6','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b1r5','5r','hb c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b2r5','5r','hb c2','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b3r5','5r','hb c3','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b4r5','5r','hb c4','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b5r5','5r','hb c5','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('b6r5','5r','hb c6','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c1r5','5r','hc c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c2r5','5r','hc c2','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c3r5','5r','hc c3','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c4r5','5r','hc c4','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c5r5','5r','hc c5','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('c6r5','5r','hc c6','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d1r5','5r','hd c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d2r5','5r','hd c2','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d3r5','5r','hd c3','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d4r5','5r','hd c4','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d5r5','5r','hd c5','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('d6r5','5r','hd c6','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e1r5','5r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e2r5','5r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e3r5','5r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e4r5','5r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e5r5','5r','he c1','thuong',65000);
+Insert into UNIVERSE.GHE (MAGHE,MARAP,VITRI,LOAIGHE,GIA) values ('e6r5','5r','he c1','thuong',65000);
+
+
+----- loai phim
+Insert into UNIVERSE.LOAIPHIM (MALP,TENLP,HINHTHUC) values ('1lp','3D','Vietsub');
+Insert into UNIVERSE.LOAIPHIM (MALP,TENLP,HINHTHUC) values ('2lp','3D','Long Tieng');
+Insert into UNIVERSE.LOAIPHIM (MALP,TENLP,HINHTHUC) values ('3lp','2D','Vietsub');
+Insert into UNIVERSE.LOAIPHIM (MALP,TENLP,HINHTHUC) values ('4lp','2D','Long Tieng');
+
+---- phim
+Insert into UNIVERSE.PHIM (MAPHIM,MALP,TENPHIM,THOILUONG,QUOCGIA) values ('2p','3lp','Aquaman',180,'USA');
+Insert into UNIVERSE.PHIM (MAPHIM,MALP,TENPHIM,THOILUONG,QUOCGIA) values ('3p','1lp','Aquaman 3D',180,'USA');
+Insert into UNIVERSE.PHIM (MAPHIM,MALP,TENPHIM,THOILUONG,QUOCGIA) values ('4p','3lp','IT',135,'ENG');
+Insert into UNIVERSE.PHIM (MAPHIM,MALP,TENPHIM,THOILUONG,QUOCGIA) values ('5p','3lp','Ky Sinh Trung',135,'KOREA');
+Insert into UNIVERSE.PHIM (MAPHIM,MALP,TENPHIM,THOILUONG,QUOCGIA) values ('1p','3lp','Mat Biec',120,'Viet Nam');
+
+
+-------- rap phim
+Insert into UNIVERSE.RAPCHIEU (MARAP,SLGHE) values ('1r',30);
+Insert into UNIVERSE.RAPCHIEU (MARAP,SLGHE) values ('2r',30);
+Insert into UNIVERSE.RAPCHIEU (MARAP,SLGHE) values ('3r',30);
+Insert into UNIVERSE.RAPCHIEU (MARAP,SLGHE) values ('4r',30);
+Insert into UNIVERSE.RAPCHIEU (MARAP,SLGHE) values ('5r',30);
+
+
+-----suat chieu--- <dang cap nhat>
+Insert into UNIVERSE.SUATCHIEU (MASC,MARAP,MAPHIM,NGAYCHIEU,THOIGIANCHIEU) values ('1sc','1r','1p',to_date('30-06-2020','dd-MM-yyyy'),'10:30');
+Insert into UNIVERSE.SUATCHIEU (MASC,MARAP,MAPHIM,NGAYCHIEU,THOIGIANCHIEU) values ('2sc','1r','1p',to_date('30-06-2020','dd-MM-yyyy'),'12:30');
+Insert into UNIVERSE.SUATCHIEU (MASC,MARAP,MAPHIM,NGAYCHIEU,THOIGIANCHIEU) values ('3sc','1r','1p',to_date('30-06-2020','dd-MM-yyyy'),'17:00');
+Insert into UNIVERSE.SUATCHIEU (MASC,MARAP,MAPHIM,NGAYCHIEU,THOIGIANCHIEU) values ('4sc','2r','2p',to_date('30-06-2020','dd-MM-yyyy'),'10:30');
+Insert into UNIVERSE.SUATCHIEU (MASC,MARAP,MAPHIM,NGAYCHIEU,THOIGIANCHIEU) values ('5sc','2r','2p',to_date('30-06-2020','dd-MM-yyyy'),'12:30');
+Insert into UNIVERSE.SUATCHIEU (MASC,MARAP,MAPHIM,NGAYCHIEU,THOIGIANCHIEU) values ('6sc','2r','2p',to_date('30-06-2020','dd-MM-yyyy'),'17:00');
+Insert into UNIVERSE.SUATCHIEU (MASC,MARAP,MAPHIM,NGAYCHIEU,THOIGIANCHIEU) values ('7sc','3r','3p',to_date('30-06-2020','dd-MM-yyyy'),'15:00');
+Insert into UNIVERSE.SUATCHIEU (MASC,MARAP,MAPHIM,NGAYCHIEU,THOIGIANCHIEU) values ('8sc','3r','3p',to_date('30-06-2020','dd-MM-yyyy'),'17:00');
+Insert into UNIVERSE.SUATCHIEU (MASC,MARAP,MAPHIM,NGAYCHIEU,THOIGIANCHIEU) values ('9sc','1r','4p',to_date('01-07-2020','dd-MM-yyyy'),'10:30');
+Insert into UNIVERSE.SUATCHIEU (MASC,MARAP,MAPHIM,NGAYCHIEU,THOIGIANCHIEU) values ('10sc','1r','4p',to_date('01-07-2020','dd-MM-yyyy'),'12:30');
+Insert into UNIVERSE.SUATCHIEU (MASC,MARAP,MAPHIM,NGAYCHIEU,THOIGIANCHIEU) values ('11sc','1r','4p',to_date('01-07-2020','dd-MM-yyyy'),'17:00');
+
+
+
+
+
